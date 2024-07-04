872410c6b6af185eeb4ffcfbc0059bbcf1de8316
cristiscu <cristiscu@gmail.com>
2024-07-04 15:04:17 -0700

    updates

--- sections/4-json-parsing/11-json-querying/3-lateral/2-queries.sql
+++
@@ -1,7 +0,0 @@
-select d.*, e.*
-  from dept d inner join emp e ON e.dept_id = d.dept_id
-  order by e.emp_id;
-
-select *
-  from dept d, lateral (select * from emp e where e.dept_id = d.dept_id) 
-  order by emp_id;
