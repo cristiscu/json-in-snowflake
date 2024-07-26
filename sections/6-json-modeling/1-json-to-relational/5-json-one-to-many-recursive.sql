eb355627bd93fa44cc03e6957db39998dc8fcbbb
cristiscu <cristiscu@gmail.com>
2024-07-26 09:19:58 -0700

    update

--- sections/6-json-modeling/1-json-to-relational/5-json-one-to-many-recursive.sql
+++ sections/6-json-modeling/1-json-to-relational/5-json-one-to-many-recursive.sql
@@ -3,20 +3,20 @@ use schema test.public;
 
 -- get all parent-child relationships, w/ recursive CTE
 with recursive cte as (
-    select v:name::string as parent, null as child, v as obj
+    select null as parent, v:name::string as child, v as path
     from employees_json
     union all
-    select child.value:name::string, cte.parent, child.value
-    from cte, table(flatten(cte.obj:employees)) child)
+    select cte.child, child.value:name::string, child.value
+    from cte, table(flatten(cte.path:employees)) child)
 select * from cte;
 
 -- create relational table from the same result
 create or replace table employees_rel(parent string, child string) as
 with recursive cte as (
-    select v:name::string as parent, null as child, v as obj
+    select null as parent, v:name::string as child, v as path
     from employees_json
     union all
-    select child.value:name::string, cte.parent, child.value
-    from cte, table(flatten(cte.obj:employees)) child)
+    select cte.child, child.value:name::string, child.value
+    from cte, table(flatten(cte.path:employees)) child)
 select parent, child from cte;
 select * from employees_rel;
